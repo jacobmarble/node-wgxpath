4a10ff5244049afecf7dc626187eadfc60b2cab1
Jacob Marble <jacobmarble@gmail.com>
2014-03-16 15:24:09 -0700

    Fix broken test.

--- word_of_the_day.js
+++ word_of_the_day.js
@@ -5,12 +5,15 @@ var url = 'http://www.merriam-webster.com/word-of-the-day/';
 var expressionString = '//*[@id="content"]/div[3]/ul/li[1]/strong';
 
 jsdom.env({
-  html: url,
+  url: url,
   done: function(errors, window) {
     wgxpath.install(window);
     var expression = window.document.createExpression(expressionString);
     var result = expression.evaluate(window.document,
         wgxpath.XPathResultType.STRING_TYPE);
-    console.log('The Word of the Day is "' + result.stringValue + '."');
+    if (result.stringValue.length <= 0)
+      console.log('Failed to get the word of the day.');
+    else
+      console.log('The Word of the Day is "' + result.stringValue + '."');
   }
 });
